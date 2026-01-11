3f261e95b67a5e2468b6ee52cfbed20ad41bffd6
Arek Femin <programming8idea@gmail.com>
2026-01-11 22:48:37 +0100

    Changed atributes href in logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import LOGO_ICON from '../../assets/cookie.svg';
 
 export const Logo = () => {
 	return (
-		<a href="/nowy-przepis" className={styles['logo-name']}>
+		<a href="/" className={styles['logo-name']}>
 			<img className={styles['logo-icon']} src={LOGO_ICON} />
 			moja<span className={styles['logo-name-color']}>DIETA</span>
 		</a>
